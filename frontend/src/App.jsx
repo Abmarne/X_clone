2175cdba00eac2f9a5e2114addc92c19f7c1a84f
Ved-sketch <vedgupta1806@gmail.com>
2025-12-19 03:07:36 +0530

    starting from scratch.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <div className="relative h-full w-full">
+      <div className="flex">
+        <div className="flex-1 min-h-screen bg-red-500"></div>
+        <div className="flex-1 min-h-screen bg-blue-500"></div>
+        <div className="flex-1 min-h-screen bg-green-500"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
