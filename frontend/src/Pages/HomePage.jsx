2f995a6e08bc8ee156cd26f555424fa94c20ad19
Ved-sketch <vedgupta1806@gmail.com>
2025-12-20 02:32:20 +0530

    added consistent spacing. Left-aligned the buttons.

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -7,85 +7,65 @@ const HomePage = () => {
         <div className="relative h-full w-full">
             <div className="flex">
                 {/* Left Side */}
-                <div className="flex-[3] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800">
-                    <div className="absolute flex flex-col items-center justify-center">
-                        <div className="m-5">
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost">
-                                    <img src={logo} alt="x_logo" className=" w-15 h-15"/>
-                                </Link>
-                            </div>
+                <div className="flex-[2] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800">
+                    <div className="flex flex-col items-start px-4 py-5">
+                        <div className="mb-5">
+                            <Link to={"/"} className="btn btn-ghost">
+                                <img src={logo} alt="x_logo" className=" w-15 h-15"/>
+                            </Link>
                         </div>
 
-                        <div className="flex items-center justify-center flex-col">
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <HouseIcon className="w-8 h-8"/>
-                                    <span className="text-xl">Home</span>
-                                </Link>
-                            </div>
+                        <div className="flex flex-col items-start w-full gap-3">
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <HouseIcon className="w-8 h-8"/>
+                                <span className="text-xl">Home</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <SearchIcon className="w-8 h-8"/>
-                                    <span className="text-xl">Explore</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <SearchIcon className="w-8 h-8"/>
+                                <span className="text-xl">Explore</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <Bell className="w-8 h-8"/>
-                                    <span className="text-xl">Notifications</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <Bell className="w-8 h-8"/>
+                                <span className="text-xl">Notifications</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <UserPlus className="w-8 h-8"/>
-                                    <span className="text-xl">Follow</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <UserPlus className="w-8 h-8"/>
+                                <span className="text-xl">Follow</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <Mail className="w-8 h-8"/>
-                                    <span className="text-xl">Chat</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <Mail className="w-8 h-8"/>
+                                <span className="text-xl">Chat</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <CircleSlash2 className="w-8 h-8"/>
-                                    <span className="text-xl">Grok</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <CircleSlash2 className="w-8 h-8"/>
+                                <span className="text-xl">Grok</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <Users className="w-8 h-8"/>
-                                    <span className="text-xl">Communities</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <Users className="w-8 h-8"/>
+                                <span className="text-xl">Communities</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <User className="w-8 h-8"/>
-                                    <span className="text-xl">Profile</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <User className="w-8 h-8"/>
+                                <span className="text-xl">Profile</span>
+                            </Link>
 
-                            <div>
-                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full h-full">
-                                    <CircleEllipsis className="w-8 h-8"/>
-                                    <span className="text-xl">More</span>
-                                </Link>
-                            </div>
+                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                <CircleEllipsis className="w-8 h-8"/>
+                                <span className="text-xl">More</span>
+                            </Link>
                         </div>
                     </div>
                 </div>
 
                 <div className="flex-[4] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800"></div>
-                <div className="flex-[4] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800"></div>
+                <div className="flex-[3] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800"></div>
             </div>
         </div>
     );
