ba201a947c8383d0ef70a7a16dac34e9ca8664a4
Ved-sketch <vedgupta1806@gmail.com>
2025-12-21 00:30:57 +0530

    added the post button on the left side panel.

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
             <div className="flex">
                 {/* Left Side */}
                 <div className="flex-[2] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800">
-                    <div className="flex flex-col items-start px-4 py-5">
+                    <div className="flex flex-col items-start px-4 py-5 gap-3">
                         <div className="mb-5">
                             <Link to={"/"} className="btn btn-ghost">
                                 <img src={logo} alt="x_logo" className=" w-15 h-15"/>
@@ -16,51 +16,74 @@ const HomePage = () => {
                         </div>
 
                         <div className="flex flex-col items-start w-full gap-3">
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <HouseIcon className="w-8 h-8"/>
-                                <span className="text-xl">Home</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <HouseIcon className="w-8 h-8"/>
+                                    <span className="text-xl">Home</span>
+                                </Link>
+                            </div>
+                            
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <SearchIcon className="w-8 h-8"/>
+                                    <span className="text-xl">Explore</span>
+                                </Link>
+                            </div>
 
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <SearchIcon className="w-8 h-8"/>
-                                <span className="text-xl">Explore</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <Bell className="w-8 h-8"/>
+                                    <span className="text-xl">Notifications</span>
+                                </Link>
+                            </div>
 
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <Bell className="w-8 h-8"/>
-                                <span className="text-xl">Notifications</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <UserPlus className="w-8 h-8"/>
+                                    <span className="text-xl">Follow</span>
+                                </Link>
+                            </div>
 
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <UserPlus className="w-8 h-8"/>
-                                <span className="text-xl">Follow</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <Mail className="w-8 h-8"/>
+                                    <span className="text-xl">Chat</span>
+                                </Link>
+                            </div>
 
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <Mail className="w-8 h-8"/>
-                                <span className="text-xl">Chat</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <CircleSlash2 className="w-8 h-8"/>
+                                    <span className="text-xl">Grok</span>
+                                </Link>
+                            </div>
 
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <CircleSlash2 className="w-8 h-8"/>
-                                <span className="text-xl">Grok</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <Users className="w-8 h-8"/>
+                                    <span className="text-xl">Communities</span>
+                                </Link>
+                            </div>
 
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <Users className="w-8 h-8"/>
-                                <span className="text-xl">Communities</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <User className="w-8 h-8"/>
+                                    <span className="text-xl">Profile</span>
+                                </Link>
+                            </div>
 
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <User className="w-8 h-8"/>
-                                <span className="text-xl">Profile</span>
-                            </Link>
-
-                            <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
-                                <CircleEllipsis className="w-8 h-8"/>
-                                <span className="text-xl">More</span>
-                            </Link>
+                            <div>
+                                <Link to={"/"} className="btn btn-ghost rounded-full p-3 w-full justify-start gap-4">
+                                    <CircleEllipsis className="w-8 h-8"/>
+                                    <span className="text-xl">More</span>
+                                </Link>
+                            </div>
                         </div>
+                         
+                        <button className="bg-white p-3 btn rounded-full btn-ghost w-[90%] h-[90%] justify-center text-black font-bold">
+                            Post
+                        </button>
+                        
                     </div>
                 </div>
 
