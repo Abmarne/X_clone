9af212914394a970adbae298e751e7d06675bf0a
Ved-sketch <vedgupta1806@gmail.com>
2025-12-24 03:04:31 +0530

    added the for you/ following buttons in the home page ui.

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -28,7 +28,7 @@ const HomePage = () => {
         <div className="relative h-full w-full">
             <div className="flex">
                 {/* Left Side */}
-                <div className="flex-[2] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800 fixed">
+                <div className="flex-[2] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800 sticky top-0">
                     <div className="flex flex-col items-start px-4 gap-2">
 
                         <div className="flex flex-col items-start px-4 py-5 gap-3">
@@ -131,15 +131,24 @@ const HomePage = () => {
                     </div>
                 </div>
 
-                <div className="flex-[4] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800"></div>
+                <div className="flex-[4] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800">
+                    <div className=" flex mt-3 font-bold">
+                        <div className="flex-1 border border-black border-b-gray-800 p-4 text-center hover:bg-gray-800 bg-transparent">
+                            <Link to={"/home"}>For you</Link>
+                        </div>
+                        <div className="flex-1 border border-black border-b-gray-800 p-4 text-center hover:bg-gray-800 bg-transparent">
+                            <Link to={"/home/following"}>Following</Link>
+                        </div>
+                    </div>
+                </div>
+                
                 {/*Right side */}
-                <div className="flex-[3] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800 sticky">
-                    <div className="h-full w-full">
-                        <div className="fixed w-full top-5">
+                <div className="flex-[3] min-h-screen bg-[rgba(0,0,0,0.0)] border border-black border-r-gray-800 sticky top-0">
+                    <div className="flex flex-col items-start px-4 py-5">
+                        <div className="w-full mt-2">
                             <input id="search" type="text" placeholder="Search"
-                            className="ml-20 w-[30%] h-10 rounded-full bg-black border border-gray-800 
+                            className="w-full h-12 rounded-full bg-black border border-gray-800 
                             focus:border-[#1D9BF0] focus:outline-none text-white placeholder:text-gray-500 px-4">
-                                
                             </input>
                         </div>
                     </div>
