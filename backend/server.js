227460243698b19a19bd4f1bf65c5571dae647e0
Ved-sketch <vedgupta1806@gmail.com>
2025-12-28 21:03:32 +0530

    made the new post prototype.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express"
-
+import PostRoute from "./Routes/PostRoute.js";
 
 const app = express();
+
+app.use("/api/post", PostRoute);
 
 app.listen(5173, () =>{
     console.log("Server started on PORT: 5173")
--- frontend/src/Components/NewPost.jsx
+++ frontend/src/Components/NewPost.jsx
@@ -1,17 +1,23 @@
+import { useState } from 'react'
 
 const NewPost = () => {
+  const [post, setPost] = useState({ content: '' })
+
   return (
     <div className="card bg-base-100">
-        <div className="card-body">
-            <div className="form-control mb-4">
+        <div className="card-body bg-black border border-black border-b-gray-800">
+            <div className="form-control mb-4 flex gap-5">
+                <img src="" alt="profile" className="w-8 h-8 rounded-full"/>
                 <input
                     type="text"
                     placeholder="What's happening?"
-                    className="input input-bordered"
+                    className="w-full bg-black text-xl outline-none border-none focus:outline-none focus:ring-0 focus:border-none"
                     value={post.content}
-
+                    onChange={(e) => setPost({ ...post, content: e.target.value })}
                 />
             </div>
+
+            <button className="bg-white p-3 btn rounded-full btn-ghost h-[90%] justify-center text-black font-bold">Post</button>
         </div>
     </div>
   )
