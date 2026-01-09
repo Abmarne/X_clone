0327f313dfacc9aa16f12f3fe78764ef2c11a563
Ved-sketch <vedgupta1806@gmail.com>
2026-01-10 00:36:01 +0530

    adjusted the button size and positioning.

--- frontend/src/Components/FollowSuggestions.jsx
+++ frontend/src/Components/FollowSuggestions.jsx
@@ -1,30 +1,34 @@
-import { Ellipsis } from "lucide-react"
+
 import { Link } from "react-router"
 
 export const FollowSuggestions = () => {
   return (
     <div>
-        <Link  className={" flex p-1 w-full h-[80%] justify-start"}>
-            
-            <img src="" alt="profile" className="w-8 h-8 rounded-full m-3"/>
-            <div className="flex-1">
-                <div className="text-sm">
-                    Name
+        <Link >
+            <div className="flex justify-between">
+
+                <div className="flex p-1">
+                    <img src="" alt="profile" className="w-8 h-8 rounded-full m-3"/>
+                    <div className="flex-1">
+                        <div className="text-sm">
+                            Name
+                        </div>
+                        <div className="text-sm text-gray-500">
+                            Username
+                        </div>
+                        <div className="text-sm">
+                            Description
+                        </div>
+                    </div>
                 </div>
-                <div className="text-sm text-gray-500">
-                    Username
-                </div>
-                <div className="text-sm">
-                    Description
+
+                <div className="flex items-center pr-3">
+                    <button className="bg-white px-6 py-2 btn rounded-full justify-center text-black font-bold">
+                        Follow
+                    </button>   
                 </div>
             </div>
-    
-            <div>
-                <button className="bg-white p-4 btn  rounded-full  justify-center text-black font-bold">
-                    Post
-                </button>   
-            </div>
-                
+                  
         </Link>
     </div>
   )
